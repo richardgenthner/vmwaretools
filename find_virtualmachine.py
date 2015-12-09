7c3fb586f1bede9572b4ce7df558107b02f1d25e
Richard Genthner <richard@guthnur.net>
2015-12-09 13:46:43 -0800

    fixes for vc timeouts and better formatting

--- find_virtualmachine.py
+++ find_virtualmachine.py
@@ -17,12 +17,11 @@ context.verify_mode = ssl.CERT_NONE
 requests.packages.urllib3.disable_warnings()
 # END Disable SSL Certificate verification
 
-### VCENTERS to skip_vcenters put in this list
-### example of a entry would vc01.exmpale.com, needs to match gethostbyaddr's results when looking up the ip
+# Added hostnames to this list to be skipped, must match reverse dns lookups
 skip_vcenters = []
 
 ### LDAP Settings
-### Adjust these settings if your using openldap and not windows domains.
+### Adjust these for openldap servers, these are currently setup for windows active directory
 windowsDomainName = ''
 userNameAttribute = 'userPrincipalName'
 ldapHost = ''
@@ -31,8 +30,14 @@ useSSL = True
 baseDN = ''
 ### Adjust this based off your own org
 ## Currently Set for Windows AD usage
+## filters based on the objects description
 searchFilter = "(&(objectCategory=computer)(description=*vcent*)(!(description=*vcenterdb:*))(!(userAccountControl:1.2.840.113556.1.4.803:=2)))"
 
+def datacenterIs(element):
+    global datacenter
+    if len(element) > 0:
+        return element[:3] == datacenter
+    return False
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -67,7 +72,7 @@ def get_args():
 
     return args
 
-def fetchVcenters(user, password):
+def fetchVcenters(user, password, datacenter):
     # LDAP settings
     global ldapHost
     global ldapPort
@@ -97,51 +102,56 @@ def fetchVcenters(user, password):
             except:
                 None
     hosts.sort()
-
+    hosts = filter(datacenterIs, hosts)
     vcenters = []
     for host in hosts:
         vcenters.append(socket.gethostbyname(str(host)))
 
     return vcenters
 
-def processVmInformation(vm):
+def processVmInformation(vm, vcenter):
     ### This funcation pulls out the important information
-    details = {'name': vm.summary.config.name,
+    print("building vm object")
+    data = {'name': vm.summary.config.name,
                'instance UUID': vm.summary.config.instanceUuid,
-               'path to VM': vm.summary.config.vmPathName,
+               'vCenter': vcenter,
                'guest OS name': vm.summary.config.guestFullName,
-               'esx host name': vm.runtime.host.name,
-               'last booted timestamp': vm.runtime.bootTime,
+               'ESX host': vm.runtime.host.name,
                'IP Address': vm.summary.guest.ipAddress,
                'Number of CPUs': vm.summary.config.numCpu,
                'Memory (MB)': vm.summary.config.memorySizeMB,
-               'overallStatus': vm.summary.overallStatus,
                'power state': vm.runtime.powerState,
+               'macAddress': vm.config.hardware.device[14].macAddress,
+               'datastore': vm.config.datastoreUrl[0].name,
+               'connectionState': vm.summary.runtime.connectionState
                }
-    return details
+    print vcenter
+    exit(1)
+    return data
 
-def findByVMName(VMName, user, password, host):
-    # form a connection...
+def findByVMName(VMName, user, password, host, vcenterName):
     try:
         si = connect.SmartConnect(host=host, user=user, pwd=password,
                               port=443, sslContext=context)
-        # doing this means you don't need to remember to disconnect your script/objects
         atexit.register(connect.Disconnect, si)
 
-        # see:
-        # http://pubs.vmware.com/vsphere-55/topic/com.vmware.wssdk.apiref.doc/vim.ServiceInstanceContent.html
-        # http://pubs.vmware.com/vsphere-55/topic/com.vmware.wssdk.apiref.doc/vim.SearchIndex.html
         search_index = si.content.searchIndex
         vm = search_index.FindByDnsName(None, VMName, True)
 
         if vm is None:
-        #    print("Could not find virtual machine '{0}'".format(VMName))
             return False
+
+        print vim.vm.device.VirtulEthernetCard
+        nics = [ x for x in vm.config.hardware.device if isinstance(x, nic)]
+        print nics
+        exit(1)
 
         print(Fore.GREEN + "Found Virtual Machine using DNS Name")
         print(Style.RESET_ALL)
-        details = processVmInformation(vm)
+
+        details = processVmInformation(vm,vcenterName)
         return details
+
     except:
         name = socket.gethostbyaddr(vcenter)
         print Fore.RED + "Failed to Connect to %s (%s)" % (name[0], host)
@@ -150,56 +160,66 @@ def findByVMName(VMName, user, password, host):
         return False
 
 
-def findByUUID(uuid, user, password, host):
-    # form a connection...
-    si = connect.SmartConnect(host=host, user=user, pwd=password,
-                              port=443, sslContext=context)
+def findByUUID(uuid, user, password, host, vcenterName):
+    try:
+        si = connect.SmartConnect(host=host, user=user, pwd=password,
+                                  port=443, sslContext=context)
+        atexit.register(connect.Disconnect, si)
+        search_index = si.content.searchIndex
+        vm = search_index.FindByUuid(None, uuid, True, True)
 
-    # doing this means you don't need to remember to disconnect your script/objects
-    atexit.register(connect.Disconnect, si)
+        if vm is None:
+            return False
 
-    # see:
-    # http://pubs.vmware.com/vsphere-55/topic/com.vmware.wssdk.apiref.doc/vim.ServiceInstanceContent.html
-    # http://pubs.vmware.com/vsphere-55/topic/com.vmware.wssdk.apiref.doc/vim.SearchIndex.html
-    search_index = si.content.searchIndex
-    vm = search_index.FindByUuid(None, uuid, True, True)
-
-    if vm is None:
+        print(Fore.GREEN + "Found Virtual Machine using UUID")
+        print(Style.RESET_ALL)
+        details = processVmInformation(vm,vcenterName)
+        return details
+    except:
+        name = socket.gethostbyaddr(vcenter)
+        print Fore.RED + "Failed to Connect to %s (%s)" % (name[0], host)
+        print(Fore.RED + "make sure host is reachable from workstation")
+        print(Style.RESET_ALL)
         return False
 
-    print(Fore.GREEN + "Found Virtual Machine using UUID")
-    print(Style.RESET_ALL)
-    details = processVmInformation(vm)
-    return details
+def getDcFromName(name):
+    return name[:3]
 
 ### main Application
 args = get_args()
 init()
-vcenters = fetchVcenters(args.user, args.password)
 vmwareUser = windowsDomainName + '\\' + args.user
+
+if args.name:
+    vmname = args.name.upper()
+    datacenter = getDcFromName(vmname)
+
+vcenters = fetchVcenters(args.user, args.password, datacenter)
 
 for vcenter in vcenters:
     try:
-        name = socket.gethostbyaddr(vcenter)
+        vcenterName = socket.gethostbyaddr(vcenter)
+        vcenterName = vcenterName[0]
     except:
-        name = vcenter
+        vcenterName = vcenter
         continue
 
-    print "Looking on {0}".format(name[0])
-    if name in skip_vcenters:
+    print "Looking on {0}".format(vcenterName)
+    if vcenterName in skip_vcenters:
         continue
     else:
         if args.name:
-            results = findByVMName(args.name, vmwareUser, args.password, vcenter)
+            results = findByVMName(vmname, vmwareUser, args.password, vcenter, vcenterName)
 
         if args.uuid:
-            results = findByUUID(args.uuid, vmwareUser, args.password, vcenter)
+            results = findByUUID(args.uuid, vmwareUser, args.password, vcenter, vcenterName)
 
-        if results == False:
-            continue
-        else:
-            details = results
+        if results:
             break
+        else:
+            continue
 
-for name, value in details.items():
-    print("{0:{width}{base}}: {1}".format(name, value, width=25, base='s'))
+print (Fore.CYAN + "VM DETAILS")
+print(Style.RESET_ALL)
+for name, value in results.items():
+        print("{0:{width}{base}}: {1}".format(name, value, width=25, base='s'))
